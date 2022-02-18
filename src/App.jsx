83bc9328bf2952f8d9ce9e9cf4f1de8d4a98a73e
miguel Talavera <miguel.talavera1995@gmail.com>
2022-02-18 13:56:23 +0100

    routes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import {BrouserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header/>
+      <routes>
+        //rutas de los componentes
+        
+      </routes>
       <footer className="App-header">
         <p>
           App made with <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React.js</a> <img src={logo} className="App-logo" alt="logo" />
         </p>
       </footer>
+      </Router>
     </div>
   );
 }
